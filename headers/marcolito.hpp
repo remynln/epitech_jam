82e1b6c95eda9befbe84cf26b972d28b04c13843
Remy <remy.noulin@epitech.eu>
2022-02-12 15:50:12 +0100

    [ADD] marcolito el bandito de la muerto de la casa blanca

---
+++ headers/marcolito.hpp
@@ -0,0 +1,44 @@
+/*
+** EPITECH PROJECT, 2022
+** epitech_jam
+** File description:
+** marcolito
+*/
+
+#ifndef MARCOLITO_HPP_
+    #define MARCOLITO_HPP_
+
+    #include <SFML/Graphics.hpp>
+    #include <SFML/Audio.hpp>
+    #include <random>
+    #include <iostream>
+    #include <vector>
+
+    class Drug {
+        public:
+            Drug();
+            void mouse_move_handle(sf::Event::MouseMoveEvent event);
+            void mouse_click_handle(sf::Event::MouseButtonEvent event);
+            void event_handler(void);
+            void hit(void);
+            void create_circle(void);
+            void display(void);
+
+
+            sf::RenderWindow window;
+            sf::Event event;
+            sf::CircleShape *circle;
+            sf::Texture arm;
+            sf::Sprite s_arm;
+            sf::Texture ser;
+            sf::Sprite s_ser;
+            sf::RectangleShape rec_out;
+            sf::RectangleShape rec_in;
+            std::vector<sf::Sprite *> hits;
+            sf::Music music;
+            int gl;
+            int fr;
+    };
+
+
+#endif /* !MARCOLITO_HPP_ */
---
+++ src/mini_games/marcolito.cpp
@@ -0,0 +1,140 @@
+/*
+** EPITECH PROJECT, 2022
+** epitech_jam
+** File description:
+** marcolito
+*/
+
+#include "marcolito.hpp"
+
+sf::CircleShape *create_circle(void)
+{
+    sf::CircleShape *shape = new sf::CircleShape(50.f);
+
+    shape->setPosition(sf::Vector2f(((std::rand() % (1100 - 127)) + 127), ((std::rand() % (716 - 183)) + 193)));
+    shape->setFillColor(sf::Color::Transparent);
+    shape->setOutlineThickness(10.f);
+    shape->setOutlineColor(sf::Color::White);
+    sf::FloatRect globalBounds = shape->getGlobalBounds();
+    shape->setOrigin(globalBounds.width / 2 , globalBounds.height / 2);
+    return shape;
+}
+
+void Drug::hit(void)
+{
+    sf::Texture *text = new sf::Texture;
+    sf::Sprite *sprite = new sf::Sprite;
+
+    text->loadFromFile("hits.png");
+    sprite->setTexture(*text);
+    sprite->setOrigin(sprite->getGlobalBounds().width / 2, sprite->getGlobalBounds().height / 2);
+    sprite->setPosition(event.mouseButton.x, event.mouseButton.y);
+    hits.push_back(sprite);
+}
+
+void Drug::mouse_click_handle(sf::Event::MouseButtonEvent event)
+{
+    sf::Rect<float> rect = circle->getGlobalBounds();
+    sf::SoundBuffer *buff = new sf::SoundBuffer;
+    sf::Sound *sound = new sf::Sound;
+    std::string aie[3] = {"ressources/aie1.ogg", "ressources/aie2.ogg", "ressources/aie3.ogg"};
+
+
+    if (rect.contains(event.x, event.y)) {
+        rec_in.setSize(sf::Vector2f(rec_in.getSize().x + 10, rec_in.getSize().y));
+        hit();
+        fr = 0;
+    } else {
+        buff->loadFromFile(aie[rand() % 3]);
+        sound->setBuffer(*buff);
+        sound->setVolume(100);
+        sound->play();
+    }
+}
+
+void Drug::mouse_move_handle(sf::Event::MouseMoveEvent event)
+{
+    s_ser.setPosition(event.x, event.y);
+}
+
+void Drug::event_handler(void)
+{
+    if (event.type == sf::Event::Closed) {
+        window.close();
+    }
+    if (event.type == sf::Event::MouseButtonPressed) {
+        mouse_click_handle(event.mouseButton);
+    }
+    if (event.type == sf::Event::MouseMoved) {
+        mouse_move_handle(event.mouseMove);
+    }
+    return;
+}
+
+
+Drug::Drug():
+window(sf::VideoMode(1200, 800), "drug addict"),
+rec_out(sf::Vector2f(300.f, 50.f)),
+rec_in(sf::Vector2f(0.f, 50.f))
+{
+    gl = 3000;
+    fr = 0;
+
+    rec_out.setPosition(1200 - 350, 100);
+    rec_in.setPosition(rec_out.getPosition());
+    rec_out.setFillColor(sf::Color::Transparent);
+    rec_out.setOutlineThickness(5.f);
+    rec_out.setOutlineColor(sf::Color::Black);
+    rec_in.setFillColor(sf::Color::Green);
+    music.openFromFile("ressources/marcolito.ogg");
+    music.setVolume(25);
+
+    window.setMouseCursorVisible(false);
+    ser.loadFromFile("ressources/seringe.png");
+    arm.loadFromFile("ressources/arm.png");
+    s_arm.setTexture(arm);
+    s_ser.setTexture(ser);
+    s_ser.setOrigin(15 , 175);
+    music.play();
+}
+
+void Drug::display(void)
+{
+    window.clear(sf::Color::Black);
+    window.draw(s_arm);
+    window.draw(*circle);
+    window.draw(rec_in);
+    window.draw(rec_out);
+    for (sf::Sprite *prout: hits) {
+        window.draw(*prout);
+    }
+    window.draw(s_ser);
+    window.display();
+}
+
+int marcolito(void)
+{
+    Drug drug;
+    std::srand(time(0));
+
+    while (drug.window.isOpen()) {
+        while(drug.window.pollEvent(drug.event)) {
+            drug.event_handler();
+        }
+        if (drug.fr == 0 || !drug.circle) {
+            drug.fr = drug.gl;
+            drug.circle = create_circle();
+        } else {
+            drug.circle->setScale(drug.circle->getScale().x - (1.00/drug.gl), drug.circle->getScale().y - (1.00/drug.gl));
+        }
+        if (drug.fr != 0) {
+            drug.fr--;
+        } else {
+            delete drug.circle;
+        }
+        if (drug.rec_in.getSize().x == 300) {
+            drug.window.close();
+        }
+        drug.display();
+    }
+}
